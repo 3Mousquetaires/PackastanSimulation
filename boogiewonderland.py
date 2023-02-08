9751555ef633f028a3af7013f2e642de84c2585a
The Damien <damien.maitre@live.fr>
2023-02-08 11:56:08 +0100

    HA ! HA ! HA !

--- boogiewonderland.py
+++ boogiewonderland.py
@@ -1,15 +1,22 @@
 import main
-import re
+import copy
+import random
 
 def LetsDanceTonight(self):
     """Shuffle les batiments. Boogie Wonderland !"""
     C = main.C
     countlist = [0 for k in range(9)]
     
+    typeset = [0, 2, 3, 4, 5, 6, 7, 8]
+    newbatlist = []
+    
     for b in C.mb.batlist:
-        if not b.type in [1, 9] :
-            countlist[b.type] += 1
-            
+        newb =  copy.deepcopy(b)
+        if newb.type != 1 and newb.type != 9 :
+            newb.type = random.choice(typeset)
+        newbatlist.append(newb)
+    
+    
     
         
-        
+       
