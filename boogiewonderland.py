4738445f8a64e490b53d0925a92986cfe4558846
The Damien <damien.maitre@live.fr>
2023-02-12 15:03:22 +0100

    holààà

--- boogiewonderland.py
+++ boogiewonderland.py
@@ -1,22 +1,39 @@
 import main
 import copy
 import random
+import os
+import json
 
-def LetsDanceTonight(self):
+def LetsDanceTonight():
     """Shuffle les batiments. Boogie Wonderland !"""
     C = main.C
-    countlist = [0 for k in range(9)]
     
     typeset = [0, 2, 3, 4, 5, 6, 7, 8]
     newbatlist = []
     
     for b in C.mb.batlist:
         newb =  copy.deepcopy(b)
-        if newb.type != 1 and newb.type != 9 :
+        if newb.type != 1 and newb.type != 9:
             newb.type = random.choice(typeset)
-        newbatlist.append(newb)
+        newbatlist.append(newb.__dict__)
+        
+    path = os.path.join(os.getcwd(), "data",  "mapshuffled")
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+    if os.name == "nt":
+        with open(os.path.join(f"{path}\\map.json"), 'w') as file:
+            file.write(json.dumps(newbatlist, sort_keys=True, indent=4 ))
+
+    else:
+        with open(os.path.join(f"{path}/map.json"), 'w') as file:
+            file.write(json.dumps(self._dumpsBatList(), sort_keys=True, indent=4 ))
+        
     
     
     
+if __name__ == "__main__":
+    LetsDanceTonight()
         
        
